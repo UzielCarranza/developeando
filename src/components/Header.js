0073ae626204f0dcdf6432acdf3520144e575618
UzielCarranza <uzielcarranzac97@gmail.com>
2024-01-16 21:10:57 -0500

    adds the structure of the hero section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-
+import {typeWriter} from "../utils/type-writer";
 
 export const CallToAction = () => {
-    //needs to render different words inside  coder tag, words need to be related to what I do
+
+
+    useEffect(() => {
+        typeWriter( ["Coder", "Developer", "Engineer", "Programmer"], "type-writer").then(r => {console.log(r)}).catch((error) => console.log(error));
+    }, []);
+
     return (
             <Container>
                 <p className="lead">Hi! My name is</p>
                 <h1 className="display-4">Uziel Carranza Charro </h1>
-
-                <h4 className="display-6"> {`</Coder>`} </h4>
+                <h4 className="display-6">{`</`}<span id="type-writer"></span><span id="cursor">|</span>{`>`}  </h4>
                 <p className="lead">  Software Developer turning ideas into reality! </p>
                 <Row>
                     <Col>
