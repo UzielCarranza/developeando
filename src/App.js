081ee3848808d8ac809141f2d3a647ced7c2417f
UzielCarranza <uzielcarranzac97@gmail.com>
2024-01-17 20:52:12 -0500

    renders the about me component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {NavBar} from "./components/NavBar";
 import {CallToAction} from "./components/Header";
+import {AboutMe} from "./components/AboutMe";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
         <CallToAction/>
+        <AboutMe/>
     </div>
   );
 }
