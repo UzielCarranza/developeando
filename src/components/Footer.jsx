0b106cb18b8f2f50defaf4dd257160974441453c
UzielCarranza <uzielcarranzac97@gmail.com>
2024-01-18 22:02:01 -0500

    adds basic footer html structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import Container from "react-bootstrap/Container";
+import {Col, Row} from "react-bootstrap";
+import {SocialMedia} from "./SocialMedia";
+
+export const Footer = () => {
+    return (
+        <footer className="mt-5">
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <h5>Get In Touch</h5>
+                        <p>
+                            I am always open to connect with everybody.
+                            Whether you have a question or just want to say hi, my inbox is always open!
+                        </p>
+                        <p>Connect With Me!</p>
+                        <div className="w-50">
+                            <SocialMedia/>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="text-center mt-3">
+                        <p>&copy; {new Date().getFullYear()} Uziel Carranza Charro. All Rights Reserved.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
