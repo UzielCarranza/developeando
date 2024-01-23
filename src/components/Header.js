30a2b83f6e8d38c5ab9e0127f90b060f54fdfc6a
UzielCarranza <uzielcarranzac97@gmail.com>
2024-01-22 20:12:56 -0500

    adds information to display on the hero section - added classes to display hero section on mobile and desktop view

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,35 @@ import {typeWriter} from "../utils/type-writer";
 import "../styles/callToAction.css";
 
 export const CallToAction = () => {
-
-
     useEffect(() => {
         typeWriter( ["Coder", "Developer", "Engineer", "Programmer"], "type-writer").then(r => {console.log(r)}).catch((error) => console.log(error));
     }, []);
 
     return (
-            <Container>
+            <Container id="hero-intro" className="p-md-5">
                 <p className="lead">Hi! My name is</p>
-                <h1 className="display-4">Uziel Carranza Charro </h1>
-                <h4 className="display-6">{`</`}<span id="type-writer"></span><span id="cursor">|</span>{`>`}  </h4>
-                <p className="lead">  Software Developer turning ideas into reality! </p>
+
+                <div className="d-flex flex-row flex-wrap align-items-center gap-3">
+                    <h1 className="display-1">Uziel Carranza Charro</h1>
+                    <h5 className="display-4">
+                        {`</`}<span id="type-writer"></span><span id="cursor">|</span>{`>`}
+                    </h5>
+                    <h4 className="display-6 subtitle-header">  Software Engineer turning ideas into reality! </h4>
+                </div>
+
+                <p className="lead">
+                    I specialize in building exceptional digital experiences. My passion lies in the creation of projects that
+                    establish a meaningful connection with users.
+                </p>
+                <p>
+                    I am currently working on migrating a website to newer technologies and (designs)!! checkout
+                    <a href="https://www.hoseandfittings.com" className="p-2" target="_blank">
+                        HoseAndFittings.com
+                    </a>
+                </p>
                 <Row>
                     <Col>
-                        <Button variant="primary" size="lg" href="#about">
+                        <Button variant="app" size="lg" href="#about-section">
                             Get to know me!
                         </Button>
                     </Col>
